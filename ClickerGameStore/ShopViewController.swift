eecfb933cf8587a5050ad29f81dc1daad2595342
Ryan Spencer <ryan.spencer26@d155.org>
2024-09-26 08:12:10 -0500

    need to update score in shop view

---
+++ ClickerGameStore/ShopViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ShopViewController.swift
+//  ClickerGameStore
+//
+//  Created by RYAN SPENCER on 9/26/24.
+//
+
+import UIKit
+
+class ShopViewController: UIViewController {
+    
+    @IBOutlet weak var pointsLabel: UILabel!
+    let alert = UIAlertController(title: "Insufficient Funds", message: "You need more points", preferredStyle: .alert)
+    let button = UIAlertAction(title: "Ok", style: .default)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        pointsLabel.text = "Points: \(AppData.points)"
+    }
+    
+    
+    @IBAction func mult2(_ sender: Any) {
+        if AppData.points < 100{
+            self.present(alert, animated: true)
+        } else {
+            AppData.points -= 100
+            AppData.mult = 2
+        }
+    }
+    
+    @IBAction func mult3(_ sender: Any) {
+        if AppData.points < 500{
+            self.present(alert, animated: true)
+        } else {
+            AppData.points -= 500
+            AppData.mult = 3
+        }
+    }
+    
+    @IBAction func mult5(_ sender: Any) {
+        if AppData.points < 1000{
+            self.present(alert, animated: true)
+        } else {
+            AppData.points -= 1000
+            AppData.mult = 5
+        }
+    }
+    
+    @IBAction func randomImages(_ sender: Any) {
+    }
+    
+    @IBAction func audio(_ sender: Any) {
+    }
+    
+}
--- ClickerGameStore/ViewController.swift
+++ ClickerGameStore/ViewController.swift
@@ -16,14 +16,21 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var scoreLabel: UILabel!
     
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        imageView.image = UIImage.pumpkin
+        imageView.isHidden = true
     }
 
     @IBAction func tapRecognized(_ sender: UITapGestureRecognizer) {
         AppData.points += 1 * AppData.mult
         updateScore()
+        imageView.isHidden = false
+        imageView.center = sender.location(in: view)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
