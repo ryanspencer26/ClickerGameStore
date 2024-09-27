1dcac428bad0fa8dfeb953b2fbc9131f26527418
Ryan Spencer <ryan.spencer26@d155.org>
2024-09-27 08:04:00 -0500

    done fr

--- ClickerGameStore/ViewController.swift
+++ ClickerGameStore/ViewController.swift
@@ -54,7 +54,7 @@ class ViewController: UIViewController {
     }
     
     func updateScore(){
-        scoreLabel.text = "\(AppData.points) points"
+        scoreLabel.text = "\(AppData.points)pts"
     }
     
 }
